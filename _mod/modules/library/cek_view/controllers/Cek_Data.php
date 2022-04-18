51795c97f7d99aed05c7b11882d94faa437015f0
johanWijanarko <webjohan123@gmail.com>
2022-04-18 23:28:42 +0700

    branch dev

---
+++ _mod/modules/library/cek_view/controllers/Cek_Data.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//NamaTbl, NmFields, NmTitle, Type, input, required, search, help, isiedit, size, label 
+//$tbl, 'id', 'id', 'int', false, false, false, true, 0, 4, 'l_id'
+
+class Cek_Data extends MY_Controller {
+
+    function init($action='list'){
+        
+    }
+}
