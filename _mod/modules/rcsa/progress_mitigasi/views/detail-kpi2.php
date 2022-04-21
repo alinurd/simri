6eb7a6d2699c195c4fb2ebd39b8d01f2aa4d0754
munurfa <munurfa01@gmail.com>
2022-04-21 13:58:53 +0700

    review kpi nambah warna

--- _mod/modules/rcsa/progress_mitigasi/views/detail-kpi2.php
+++ _mod/modules/rcsa/progress_mitigasi/views/detail-kpi2.php
@@ -7,12 +7,14 @@
             <th width="5%" rowspan="2">No.</th>
             <th rowspan="2">Parameter</th>
             <th rowspan="2" width="8%">Satuan</th>
-            <th colspan="3" width="15%">Keterangan</th>
+            <th colspan="5" width="15%">Keterangan</th>
         </tr>
         <tr class="text-center">
-            <th class="bg-danger-400">Merah</th>
-            <th class="bg-orange-400">Kuning</th>
-            <th class="bg-success-400">Hijau</th>
+            <th class="bg-dangerx-400" style="background-color:#e70808">Merah</th>
+            <th class="bg-orangex-400" style="background-color:#f0ca0f">Orange</th>
+            <th class="bg-successx-400" style="background-color:#edfd17">Kuning</th>
+            <th class="bg-successx-400" style="background-color:#50ca4e">Hijau</th>
+            <th class="bg-successx-400" style="background-color:#2c5b29">Hijau Tua</th>
         </tr>
     </thead>
     <tbody>
@@ -27,6 +29,8 @@
             <td><?=++$no;?></td>
             <td><?=$row['title'];?></td>
             <td><?=$row['satuan'];?></td>
+            <td><?=$row['p_5'].' ['.$row['s_5_min'].' &#8805; '.$row['s_5_max'].']';?></td>
+            <td><?=$row['p_4'].' ['.$row['s_4_min'].' &#8805; '.$row['s_4_max'].']';?></td>
             <td><?=$row['p_3'].' ['.$row['s_3_min'].' &#8805; '.$row['s_3_max'].']';?></td>
             <td><?=$row['p_2'].' ['.$row['s_2_min'].' &#8805; '.$row['s_2_max'].']';?></td>
             <td><?=$row['p_1'].' ['.$row['s_1_min'].' &#8805; '.$row['s_1_max'].']';?></td>
@@ -40,9 +44,12 @@
                 <td>&nbsp;</td>
                 <td><?=$huruf.'. '.$row_det['title'];?></td>
                 <td><?=$row_det['satuan'];?></td>
+                <td><?=$row_det['p_5'].' ['.$row_det['s_5_min'].' &#8805; '.$row_det['s_5_max'].']';?></td>
+                <td><?=$row_det['p_4'].' ['.$row_det['s_4_min'].' &#8805; '.$row_det['s_4_max'].']';?></td>
                 <td><?=$row_det['p_3'].' ['.$row_det['s_3_min'].' &#8805; '.$row_det['s_3_max'].']';?></td>
                 <td><?=$row_det['p_2'].' ['.$row_det['s_2_min'].' &#8805; '.$row_det['s_2_max'].']';?></td>
                 <td><?=$row_det['p_1'].' ['.$row_det['s_1_min'].' &#8805; '.$row_det['s_1_max'].']';?></td>
+            
                 </tr>
             <?php endforeach;
             
