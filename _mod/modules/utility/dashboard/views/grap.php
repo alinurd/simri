06d796fc69d42f3f6825d4171680ea6852feb56c
munurfa <munurfa01@gmail.com>
2022-06-24 12:23:48 +0700

    export and grap

--- _mod/modules/utility/dashboard/views/grap.php
+++ _mod/modules/utility/dashboard/views/grap.php
@@ -1,28 +1,32 @@
 <div class="chart has-fixed-height" id="pie_basic"></div>
-
 <?php
-    $resultD=[];
-    foreach($data as $key=>$row){
-        $resultD[]=['value'=>$row['nilai'], 'name'=>$row['category'], 'type_chat'=>1, 'param_id'=>$key];
-    }
-    $x['data']=$resultD;
+    $resultD = [];
+    $resultD[] = ['value' => $data['110'], 'name' => 'Sebelum Due Date (110%)', 'type_chat' => 2, 'param_id' => 4];
+    $resultD[] = ['value' => $data['100'], 'name' => 'On Schedule (100%)', 'type_chat' => 2, 'param_id' => 3];
+    $resultD[] = ['value' => $data['90'], 'name' => 'Delay 1 month  (90%)', 'type_chat' => 2, 'param_id' => 2];
+    $resultD[] = ['value' => $data['75'], 'name' => 'Delay &#62; 1 month(75%)', 'type_chat' => 2, 'param_id' => 1];
+    $resultD[] = ['value' => $data['0'], 'name' => 'Tidak terlaksana(0%)', 'type_chat' => 2, 'param_id' => 0];
 
-    $x['title']=[
-                'text'=>'Pelaksanaan Mitigasi',
-                'subtext'=>'Last updated: '.date('M d, Y, H:i'),
-                'left'=>'center',
-                'textStyle'=>[
-                    'fontSize'=>17,
-                    'fontWeight'=>500
-                ],
-                'subtextStyle'=> [
-                    'fontSize'=>12
-                ]
-            ];
-    $x['warna']=[$this->_preference_['warna_mitigasi_selesai'],$this->_preference_['warna_mitigasi_belum_on_schedule'],$this->_preference_['warna_mitigasi_belum_terlambat'],$this->_preference_['warna_mitigasi_belum_dilaksanakan']];
-    $hasil=json_encode($x);
+
+    $x['data'] = $resultD;
+    $x['title'] = [
+        'text' => 'Tingkat Pelaksanaan and Efektivitas Mitigasi Risiko',
+        'subtext' => 'Last updated: ' . date('M d, Y, H:i'),
+        'left' => 'center',
+        'textStyle' => [
+            'fontSize' => 17,
+            'fontWeight' => 500
+        ],
+        'subtextStyle' => [
+            'fontSize' => 12
+        ]
+    ];
+    $x['warna'] = ['#23890f', '#1460d1', '#7e57c2', '#ff0000', '#009688'];
+    $hasil = json_encode($x);
+
 ?>
+
 <script>
-    var data=<?=$hasil;?>;
+    var data = <?= $hasil; ?>;
     grafik_pie(data, 'pie_basic');
 </script>
