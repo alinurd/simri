766d284c72fa661bf65b843293957afcf9d8627e
xortnet <ricky.property22@gmail.com>
2024-07-22 16:55:03 +0700

    update
    - validate due date rencana mitigasi

--- _mod/modules/rcsa/risk_context/views/mitigasi.php
+++ _mod/modules/rcsa/risk_context/views/mitigasi.php
@@ -1,45 +1,51 @@
 <div id="entri_mitigasi">
     <?php
-    foreach($mitigasi as $key=>$row):
-        $add=false;
-        $help='';
-        $mandatori=false;
-        if (isset($row['mandatori'])){
-            $mandatori=$row['mandatori'];
+    foreach( $mitigasi as $key => $row ) :
+        $add       = FALSE;
+        $help      = '';
+        $mandatori = FALSE;
+        if( isset( $row['mandatori'] ) )
+        {
+            $mandatori = $row['mandatori'];
         }
-        $required='';
-        if ($mandatori){
-            $required='<sup class="text-danger">*)</sup>&nbsp;&nbsp;';
+        $required = '';
+        if( $mandatori )
+        {
+            $required = '<sup class="text-danger">*)</sup>&nbsp;&nbsp;';
         }
 
-        if (isset($row['add']))
-            $add=$row['add'];
-        if (isset($row['help']))
-            $help=$row['help'];
+        if( isset( $row['add'] ) )
+            $add = $row['add'];
+        if( isset( $row['help'] ) )
+            $help = $row['help'];
 
-        $help_popup=true;
-        if (isset($row['help_popup'])){
-            $help_popup=$row['help_popup'];
+        $help_popup = TRUE;
+        if( isset( $row['help_popup'] ) )
+        {
+            $help_popup = $row['help_popup'];
         }
-        $br='';
-        if (!$help_popup){
-            $br='<br/>';
+        $br = '';
+        if( ! $help_popup )
+        {
+            $br = '<br/>';
         }
-        
+
         ?>
         <div class="form-group row">
-            <label class="col-lg-3 col-form-label text-<?=$this->_preference_['align_label'];?>"><?=$required.$row['title'].$br.$help;?></label>
+            <label
+                class="col-lg-3 col-form-label text-<?= $this->_preference_['align_label']; ?>"><?= $required . $row['title'] . $br . $help; ?></label>
             <div class="col-lg-9">
-                <div class ="form-group form-group-feedback form-group-feedback-right input-group">
-                    <?=$row['isi'];?>
+                <div class="form-group form-group-feedback form-group-feedback-right input-group">
+                    <?= $row['isi']; ?>
                 </div>
             </div>
         </div>
         <?php
-    endforeach;?>
-    <span class="btn bg-success-400 btn-labeled btn-labeled-right legitRipple pull-right" id="simpan_mitigasi"><b><i class="icon-floppy-disk "></i></b> <?=_l('fld_save_mitigasi');?></span>
-    <br/>
-    <hr/>
+    endforeach; ?>
+    <span class="btn bg-success-400 btn-labeled btn-labeled-right legitRipple pull-right" id="simpan_mitigasi"><b><i
+                class="icon-floppy-disk "></i></b> <?= _l( 'fld_save_mitigasi' ); ?></span>
+    <br />
+    <hr />
 </div>
 <script>
     $(function () {
@@ -49,10 +55,11 @@
         });
         $('.rupiah').number(true);
         $('[data-popup="tooltip"]').tooltip();
-        $('.pickadate').pickadate({
+        $('.pickadateaddmitigasi').pickadate({
             selectMonths: true,
-            selectYears: true,
+            selectYears: false,
             formatSubmit: 'yyyy/mm/dd',
+            min: [new Date().getFullYear(), 0, 01]
         });
     })
 </script>
