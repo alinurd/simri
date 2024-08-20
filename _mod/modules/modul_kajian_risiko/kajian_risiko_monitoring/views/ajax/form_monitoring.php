6a4b7f59fcb89bc7346174fc28596dac3f712c2b
xortnet <ricky.property22@gmail.com>
2024-08-20 15:14:17 +0700

    update

--- _mod/modules/modul_kajian_risiko/kajian_risiko_monitoring/views/ajax/form_monitoring.php
+++ _mod/modules/modul_kajian_risiko/kajian_risiko_monitoring/views/ajax/form_monitoring.php
@@ -79,11 +79,11 @@
                                                     for="dokumen_pendukung">Choose
                                                     file</label>
                                             </div>
-                                            <div class="mt-2">
-                                                <label style="color:red;font-size:10px"><i>*) Supported File :
-                                                        pdf, doc, docx, xls, xlsx</i>
-                                                </label>
-                                            </div>
+                                        </div>
+                                        <div class="mt-2">
+                                            <label style="color:red;font-size:10px"><i>*) Supported File :
+                                                    pdf, doc, docx, xls, xlsx</i>
+                                            </label>
                                         </div>
                                         <input type="hidden" name="file_monitoring" id="file-monitoring"
                                             value="<?= ( ! empty( $formdata["dokumen_pendukung"] ) ? $formdata["dokumen_pendukung"] : "" ) ?>"
