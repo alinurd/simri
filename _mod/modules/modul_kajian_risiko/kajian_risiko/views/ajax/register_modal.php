1ce63ed1b1daef86d5eef44c94c8a2f68135a375
ali dev14 <ali.nrdn14005@gmail.com>
2024-10-16 04:00:54 +0700

    tukar posisi modal register

--- _mod/modules/modul_kajian_risiko/kajian_risiko/views/ajax/register_modal.php
+++ _mod/modules/modul_kajian_risiko/kajian_risiko/views/ajax/register_modal.php
@@ -44,18 +44,18 @@
                                                 <td><?= $vRegister["tipe_risiko_name"] ?></td>
                                                 <td class="text-center"><?= $vRegister["current_level_likelihood_text"] ?></td>
                                                 <td class="text-center"><?= $vRegister["current_level_impact_text"] ?></td>
-                                                <td class="text-center p-1">
-                                                    <div class="alert alert-sm border shadow-none m-0 p-1"
-                                                        style="background-color:<?= $vRegister["current_level_color"] ?>;color:<?= $vRegister["current_text_level_color"] ?>">
-                                                        <b><?= $vRegister["current_level_name"] ?></b>
-                                                    </div>
-                                                </td>
                                                 <td class="text-center"><?= $vRegister["residual_level_likelihood_text"] ?></td>
                                                 <td class="text-center"><?= $vRegister["residual_level_impact_text"] ?></td>
                                                 <td class="text-center  p-1">
                                                     <div class="alert alert-sm border shadow-none m-0 p-1"
                                                         style="background-color:<?= $vRegister["residual_level_color"] ?>;color:<?= $vRegister["residual_text_level_color"] ?>">
                                                         <b><?= $vRegister["residual_level_name"] ?></b>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center p-1">
+                                                    <div class="alert alert-sm border shadow-none m-0 p-1"
+                                                        style="background-color:<?= $vRegister["current_level_color"] ?>;color:<?= $vRegister["current_text_level_color"] ?>">
+                                                        <b><?= $vRegister["current_level_name"] ?></b>
                                                     </div>
                                                 </td>
                                             </tr>
