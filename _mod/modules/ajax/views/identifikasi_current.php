802b6d710e873d150fe180ea40120d0de6120645
ali dev14 <ali.nrdn14005@gmail.com>
2024-10-23 02:50:50 +0700

    dashboard

--- _mod/modules/ajax/views/identifikasi_current.php
+++ _mod/modules/ajax/views/identifikasi_current.php
@@ -20,8 +20,11 @@
         // doi::dump($pos);
         $no = 0;
         foreach( $detail as $row ) :
-            // $this->db->select('color, color_text, level_color, like, impact, month');
-            $this->db->where( 'risiko_target_mon', $pos['id'] );
+             // $this->db->select('color, color_text, level_color, like, impact, month');
+            $scr = $this->db->where('impact_code', $row['impact_code'])->where('like_code', $row['like_code'])->get("il_view_level_mapping")->row_array();
+            $scrMon = $this->db->where('impact_code', $row['impact_code_mon'])->where('like_code', $row['like_code_mon'])->get("il_view_level_mapping")->row_array();
+            $scrTar = $this->db->where('impact_code', $row['impact_code_target'])->where('like_code', $row['like_code_target'])->get("il_view_level_mapping")->row_array();
+             $this->db->where( 'risiko_target_mon', $pos['id'] );
             // $this->db->order_by('month', 'DESC');  
             $this->db->limit( 1 );
             $r = $this->db->get( "il_view_rcsa_detail_monitoring" )->row_array();
@@ -33,18 +36,18 @@
                 <td><?= $row['risiko_dept']; ?></td>
                 <td><?= $row['klasifikasi_risiko'] . ' | ' . $row['tipe_risiko']; ?></td>
                 <td class="text-center" style="background-color:<?= $row['color']; ?>;color:<?= $row['color_text']; ?>;">
-                    <?= $row['level_color']; ?><br /><small><?= $row['like_code'] . 'x' . $row['impact_code'] . ' : ' . $row['risiko_inherent_text']; ?></small>
+                    <?= $row['level_color']; ?><br /><small><?= $row['like_code'] . 'x' . $row['impact_code'] . ' : ' . $scr['score']; ?></small>
                 </td>
 
                 <td class="text-center"
                     style="background-color:<?= $row['color_mon']; ?>;color:<?= $row['color_text_mon']; ?>;">
-                    <?= $row['level_color_mon']; ?><br /><small><?= $row['like_code_mon'] . 'x' . $row['impact_code_mon'] . ' : ' . $row['like_code_mon'] * $row['impact_code_mon']; ?></small>
+                    <?= $row['level_color_mon']; ?><br /><small><?= $row['like_code_mon'] . 'x' . $row['impact_code_mon'] . ' : ' . $scrMon['score']; ?></small>
                 </td>
 
 
                 <td class="text-center"
                     style="background-color:<?= $row['color_target']; ?>;color:<?= $row['color_text_target']; ?>;">
-                    <?= $row['level_color_target']; ?><br /><small><?= $row['like_code_target'] . 'x' . $row['impact_code_target'] . ' : ' . $row['risiko_target_text']; ?></small>
+                    <?= $row['level_color_target']; ?><br /><small><?= $row['like_code_target'] . 'x' . $row['impact_code_target'] . ' : ' . $scrTar['score']; ?></small>
                 </td>
                 <td class="text-center"><span
                         class="badge bg-orange-400 badge-pill align-self-center ml-auto"><?= isset( $row['jml'] ) ? $row['jml'] : 0; ?></span>
